916a49f00497f63172cd6f3ef48547dfcf695037
ngolam111019 <ngothanhlamit@gmail.com>
2025-06-12 19:03:11 +0700

    edit template mail đăng ký tài khoản thành công

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,4 +1,7 @@
 const nodemailer = require('nodemailer');
+const fs = require('fs');
+const path = require('path');
+const Mustache = require('mustache');
 
 var transporter = nodemailer.createTransport({ // config mail server
     host: "smtp.hostinger.com",
@@ -35,12 +38,20 @@ async function sendOtpEmail(to, otp, subject) {
 }
 
 async function sendEmailDangKyThanhCong(to, pass) {
+
+
+  // Đọc template HTML từ file
+  const templatePath = path.join(__dirname, '../mail_templates/dangky-thanhcong.html');
+  const template = fs.readFileSync(templatePath, 'utf-8');
+
+  // Render HTML với password
+  const htmlContent = Mustache.render(template, { email: to, password: pass });
+
   const mailOptions = {
     from: `"Tool AI" <${process.env.EMAIL_USER}>`,
     to,
     subject: 'Tài khoản Tool AI đã được tạo',
-    html: `<p>Chúc mừng bạn đã đăng ký thành công.</p>
-           <p>Mật khẩu đăng nhập là: <strong>${pass}</strong></p>`
+    html: htmlContent
   };
   
   return transporter.sendMail(mailOptions);
